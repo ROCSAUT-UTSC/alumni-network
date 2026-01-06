abbc9c6df81ae79b560aa367945d72ac3722a781
Arvin Yang <arvinandkabone@Arvins-Mac.local>
2026-01-06 09:42:24 -0500

    basic data structure for User and Alumni

---
+++ backend/app/db/pydantic.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class Products(BaseModel):
+  id: int
+  name: str
+  description: str
+  price: float
+  quantity: int
+
+class Customer(BaseModel):
+  id: int
+  name: str
+  product_id : int
